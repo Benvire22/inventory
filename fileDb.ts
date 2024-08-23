03a0599abe04a368e8430087bebf16b55a5c5fd2
Limarev Ivan <ivantaketo@gmail.com>
2024-08-23 23:34:04 +0600

    Add deleteItem and getOneItem routes.

--- fileDb.ts
+++ fileDb.ts
@@ -33,6 +33,12 @@ const fileDb = {
   async getItems() {
     return data.items;
   },
+  async getCategories() {
+    return data.categories;
+  },
+  async getPlaces() {
+    return data.places;
+  },
   async addItem(item: ItemMutation) {
     const inventoryItem: Item = {
       id: randomUUID(),
@@ -46,6 +52,37 @@ const fileDb = {
   },
   async save() {
     await fs.writeFile(fileName, JSON.stringify(data, null, 2));
+  },
+  async deleteItem(itemId: string) {
+    const items = data.items;
+    const index = items.findIndex((item) => item.id === itemId);
+
+    if (index > -1) {
+      data.items.splice(index, 1);
+
+      await this.save();
+      return items[index];
+    } else {
+      return null;
+    }
+  },
+  async findItem(currentType: string, type: string): Promise<Category | Place | null> {
+    if (type === 'category') {
+      const categories = data.categories;
+      const currentCategory = categories.filter(
+        (category) => category.title === currentType,
+      );
+
+      return currentCategory[0];
+    } else if (type === 'place') {
+      const places = data.places;
+      const currentPlace = places.filter(
+        (place) => place.title === currentType,
+      );
+      return currentPlace[0];
+    } else {
+      return null;
+    }
   },
 };
 
--- routers/inventories.ts
+++ routers/inventories.ts
@@ -10,21 +10,69 @@ inventoriesRouter.get('/', async (_, res) => {
   return res.send(items);
 });
 
+inventoriesRouter.get('/:id', async (req, res) => {
+  const items = await fileDb.getItems();
+  const item = items.find((item) => item.id === req.params.id);
+
+  if (!item) {
+    return res.status(404).send({ error: 'Item is not defined!' });
+  }
+
+  return res.send(item);
+});
+
 inventoriesRouter.post('/', imageUpload.single('image'), async (req, res) => {
-  if (!req.body.title || !req.body.place || req.body.category) {
-    return res.status(400).send({error: 'Title, place or category are required!'});
+  let categoryId = '';
+  let placeId = '';
+
+  if (!req.body.title || !req.body.place || !req.body.category) {
+    return res
+      .status(400)
+      .send({ error: 'Title, place or category are required!' });
+  } else if (req.body.place && req.body.category) {
+    const category = req.body.category;
+    const place = req.body.place;
+
+    const currentCategory = await fileDb.findItem(category, 'category');
+    const currentPlace = await fileDb.findItem(place, 'place');
+
+    if (!currentCategory || !currentPlace) {
+      return res
+        .status(400)
+        .send({ error: 'Category or place is not defined!' });
+    }
+
+    categoryId = currentCategory.id;
+    placeId = currentPlace.id;
   }
 
   const item: ItemMutation = {
-    categoryId: req.body.category,
-    placeId: req.body.place,
+    categoryId,
+    placeId,
     title: req.body.title,
-    description: req.body.description,
+    description: req.body.description || null,
     image: req.file ? req.file.filename : null,
   };
 
   const savedItem = await fileDb.addItem(item);
   return res.send(savedItem);
+});
+
+inventoriesRouter.delete('/:id', async (req, res) => {
+  let itemId = req.params.id;
+  if (itemId) {
+    const deletedItem = await fileDb.deleteItem(itemId);
+
+    if (!deletedItem) {
+      return res
+        .status(400)
+        .send({ error: 'Item has not deleted or is not defined!' });
+    }
+
+    return res.send(deletedItem);
+  } else {
+    return res.status(400).send({ error: 'Item is not defined!' });
+  }
 });
 
 export default inventoriesRouter;
